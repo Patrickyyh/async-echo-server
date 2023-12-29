c0e6f2122856a43fa8e608acd5d567778bd37082
Patrickyyh <yeyuhao1234@tamu.edu>
2023-12-29 10:49:41 +0000

    version:2  implement the session share pointer management

--- Session.cpp
+++ Session.cpp
@@ -50,26 +50,41 @@ _acceptor(ioc , tcp::endpoint(tcp::v4() , port)
  void Server::start_accept(){
     
     // Create a new session
-    Session * new_session = new Session(_ioc);
+    std::shared_ptr<Session> new_session = std::make_shared<Session>(_ioc, this);
     
     // accept the connecion with async_accept
+    
 
     /*
         The socket into which the new connection will be accepted. Ownership of the peer object is retained by the 
         caller,  which must guarantee that it is valid until the completion handler is called
     */
+
+    /*
+        Notice that since we bind the new_session with the handle_accept function,
+        the reference count of the new_session will be incremented.
+        1. As long as the bind function is in the event queue of the asio. Its lifetime will be retained.
+        2. However, if the handle_accept function has been called by asio and it is removed from the event-queue 
+        then the function returned by bind will be deallocated, the reference count will also be decremented.
+        3. So we could generate an unique session id and add the session with the id into the map of the server.
+    */
     _acceptor.async_accept(new_session->Socket() , 
-                          std::bind(&Server::handle_accept,this,new_session, std::placeholders::_1));
+                          std::bind(&Server::handle_accept,this, new_session, std::placeholders::_1));
 
  }
 
- void Server::handle_accept(Session * new_session , const boost::system::error_code & error){
+ void Server::handle_accept(std::shared_ptr<Session> new_session , const boost::system::error_code & error){
     
     // handle one accept
     if(!error){
+        
         new_session->Start();
+        
+        // put the session into the map
+        _session_collection.insert(std::make_pair(new_session->get_uuid() , new_session));
+
     }else {
-        delete new_session;
+        // delete new_session;
     }
 
     // accept new connection after handling one accept
--- Session.h
+++ Session.h
@@ -3,19 +3,32 @@
 #include "boost/asio.hpp"
 #include <iostream>
 #include <queue>
+#include <memory>
+#include "boost/uuid/uuid_generators.hpp"
+#include "boost/uuid/uuid_io.hpp"
+
 using namespace boost;
 using boost::asio::ip::tcp;
 using namespace std;
 const int RECVSIZE = 1024;
+class Server; 
+
 class Session{
     public:
-        Session(boost::asio::io_context& ioc):_socket(ioc){
-            
+        Session(boost::asio::io_context& ioc, Server * server):_socket(ioc),_server(server){
+            //generate an uuid when a session is created
+            boost::uuids::uuid session_id = boost::uuids::random_generator()();
+            _uuid =boost::uuids::to_string(session_id);
         }
 
     //Getter function for the socket
     tcp::socket & Socket(){
         return this->_socket;
+    }
+
+    //Getter function for the uuid
+    std::string & get_uuid(){
+        return this->_uuid;
     }
 
     /**
@@ -37,6 +50,8 @@ class Session{
 
         //write data back to the client (callback function)
         void handle_write(const boost::system::error_code & error);
+        Server * _server;
+        std::string _uuid;
 
 };
 
@@ -50,9 +65,13 @@ class Server{
         void start_accept();
 
         // callback function after the connection has been accepted
-        void handle_accept(Session * new_session , const boost::system::error_code & error);
+        void handle_accept(std::shared_ptr<Session> new_session , const boost::system::error_code & error);
 
         // Since the io_context could not be copied and only could be referenced 
         boost::asio::io_context & _ioc;
         boost::asio::ip::tcp::acceptor _acceptor;
+
+        // management of the session ptr
+        std::map<std::string, std::shared_ptr<Session>> _session_collection;
+
 };
