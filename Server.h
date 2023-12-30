b0bb3a8a41ff9ec05e8a95b71ee32fa3227ac157
Patrickyyh <yeyuhao1234@tamu.edu>
2023-12-30 06:53:15 -0600

    version:2 change the makefile to make it compileable on both macos and linux

--- Server.h
+++ Server.h
@@ -6,7 +6,6 @@
 class Server{
     public:
      Server(boost::asio::io_context & ioc , short port);
-
      // cleare the session
      void clear_session(std::string uuid);
 
@@ -17,7 +16,7 @@ class Server{
         // callback function after the connection has been accepted
         void handle_accept(std::shared_ptr<Session> new_session , const boost::system::error_code & error);
 
-        // Since the io_context could not be copied and only could be referenced 
+        // Since the io_context could not be copied and only could be referenced
         boost::asio::io_context & _ioc;
         boost::asio::ip::tcp::acceptor _acceptor;
 
